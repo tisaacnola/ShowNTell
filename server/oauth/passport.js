37ddf774c1c081512ef221ab4ddbaba0cc6ebd57
Billy <nbilly319@gmail.com>
2020-11-29 13:24:00 -0600

    (update/add) Schema, nav bar

--- server/oauth/passport.js
+++ server/oauth/passport.js
@@ -5,15 +5,16 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/"
-  },
-  (accessToken, refreshToken, profile, done) => {
-    User.findOrCreate({ googleId: profile.id }, (err, user) => {
-      return done(err, user);
-    });
-  }
-));
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+},
+(accessToken, refreshToken, profile, done) => {
+  // console.log(profile)
+  done(null, profile);
+  // User.findOrCreate({ googleId: profile.id }, (err, user) => {
+  //   return done(err, user);
+  // });
+}));
 
 module.exports = {
   GoogleStrategy,
-}
+};
