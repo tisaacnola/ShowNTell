8e0c86168b209be25e48e110b00be3728df7eb01
Andrew Gremillion <agremillion15@gmail.com>
2020-11-30 15:53:09 -0600

    (add) view changer function

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,19 +1,38 @@
+/* eslint-disable import/extensions */
+/* eslint-disable consistent-return */
 import React, { useState } from 'react';
 import axios from 'axios';
 import Nav from './nav.jsx';
+import Sub from './sub.jsx';
+import Post from './post.jsx';
 
 const App = () => {
   const [user, setUser] = useState();
+  const [view, setView] = useState('login');
+
   const getUser = () => {
     axios.get('/user')
       .then(({ data }) => setUser(data));
   };
   getUser();
+
+  const changeView = (newView) => {
+    setView(newView);
+  };
+
+  const getView = () => {
+    if (view === 'sub') {
+      return <Sub />;
+    }
+    if (view === 'post') {
+      return <Post />;
+    }
+  };
+
   return (
     <div>
-      {
-      user
-        ? <Nav user={user} />
+      {user
+        ? <Nav user={user} onClick={changeView} />
         : (
           <a
             href="/auth/google"
@@ -21,8 +40,8 @@ const App = () => {
           >
             login with google
           </a>
-        )
-    }
+        )}
+      {getView()}
     </div>
   );
 };
