f7f6b19d32dff01aa20f0a592d5bfcca17cbd6ff
Teamer Tibebu <teamertibebu@gmail.com>
2020-11-30 17:33:58 -0600

    (update) homeFeed to show all posts

--- client/src/components/feedItem.jsx
+++ client/src/components/feedItem.jsx
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
 const FeedItem = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const mainDiv = {
+    display: 'flex',
+    flexDirection: 'column',
+    border: '3px solid black',
+    margin: '10px',
+    boxShadow: '5px 5px #888888',
+  };
+
+  const changeLiked = () => setLiked(!liked);
+
   return (
-    <div>
-      <div>{post.title}</div>
-      <div>{post.content}</div>
-      <div>{post.show}</div>
-      <div>{post.user}</div>
+    <div style={mainDiv}>
+      <div>
+        <h4>
+          Posted By: {post.user} in {post.show}
+        </h4>
+      </div>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+      {liked ? (
+        <button
+          onClick={changeLiked}
+          style={{ width: '10%', margin: '10px', backgroundColor: 'orange' }}
+        >
+          Liked
+        </button>
+      ) : (
+        <button onClick={changeLiked} style={{ width: '10%', margin: '10px' }}>
+          Like
+        </button>
+      )}
     </div>
   );
 };
