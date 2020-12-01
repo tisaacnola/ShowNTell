709bc42d237707a394a81ab2c39d139caba091f8
Andrew Gremillion <agremillion15@gmail.com>
2020-11-30 20:29:40 -0600

    (add) post request to server which saves posts to the db

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 require('dotenv').config();
 require('./db/index');
 const { GoogleStrategy } = require('./oauth/passport');
-const { Users } = require('./db/schema.js');
+const { Users, Posts, Shows } = require('./db/schema.js');
 // const { session } = require('passport');
 
 const app = express();
@@ -60,20 +60,47 @@ app.get('/auth/google/callback', passport.authenticate('google', { failureRedire
 });
 
 app.get('/user', (req, res) => {
-  res.json(userInfo);
+  res.status(200).json(userInfo);
 });
 
-app.get('/database', (req, res) => {
-  Users.find().then(data => res.json(data));
+app.get('/users', (req, res) => {
+  Users.find().then((data) => res.status(200).json(data));
+});
+
+app.get('/posts', (req, res) => {
+  Posts.find().then((data) => res.status(200).json(data));
+});
+
+app.get('/shows', (req, res) => {
+  Shows.find().then((data) => res.status(200).json(data));
 });
 
 app.get('/delete', (req, res) => {
-  Users.deleteMany().then(() => res.json('done'));
+  Users.deleteMany()
+    .then(() => Posts.deleteMany())
+    .then(() => Shows.deleteMany())
+    .then(() => res.status(200).json('done'));
 });
 
 app.get('/logout', (req, res) => {
   userInfo = null;
-  res.json(userInfo);
+  res.status(200).json(userInfo);
+});
+
+app.post('/posts', (req, res) => {
+  const { title, content, poster, show } = req.body;
+  return Posts.create({
+    title, content, user: poster, show, comments: {},
+  }).then((post) => {
+    Users.findById(poster)
+      .then((user) => {
+        Users.updateOne({ _id: poster }, { posts: [...user.posts, post._id] })
+          .catch();
+      })
+      .catch();
+  })
+    .then(() => res.status(201).send())
+    .catch(() => res.status(500).send());
 });
 
 app.listen(3000, () => {
