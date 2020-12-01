013dfdbbfd8fec7e90927f78c54581e1403e063c
Amber Khan <4access2amber@gmail.com>
2020-12-01 12:39:05 -0600

    (update) homepage and navbar

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -4,30 +4,45 @@
 /* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
 import axios from 'axios';
+import HomePage from './HomePage/HomePage.jsx';
 import DMs from './dms.jsx';
 import Notifs from './notifs.jsx';
+import logo from './HomePage/img/logo.jpg';
+import { FaHome, FaStar, FaPen, FaBell, FaEnvelope, FaSearch, FaDoorOpen } from 'react-icons/fa';
 
 const Nav = ({ user }) => {
   const [page, setPage] = useState('home');
   const loadBody = () => {
     if (page === 'home') {
-      return (<h1>home page</h1>);
+      return <HomePage />;
     } if (page === 'DMs') {
       return <DMs user={user} />;
     } if (page === 'notifs') {
       return <Notifs />;
     }
   };
+
   return (
     <div>
-      <h1>{`hello ${user.name}`}</h1>
-      <button>Home</button>
-      <button>list of subs</button>
-      <button>post</button>
-      <button id={(page === 'notifs') ? 'button' : null} onClick={() => setPage('notifs')}>notifs</button>
-      <button id={(page === 'DMs') ? 'button' : null} onClick={() => setPage('DMs')}>DMs</button>
-      <input />
-      <button onClick={() => axios.get('/logout')}>logout</button>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet"></link>
+      <img id="homeButton" src={logo} alt="logo" onClick={() => { console.log('hey'); setPage('home'); }} />
+      <ul className="navbar">
+        <li title="home feed"> {<FaHome />}  </li>
+        <li title="subscriptions"> {<FaStar />}</li>
+        <li title="create a post"> {<FaPen />} </li>
+        <li title="notifications" id={(page === 'notifs') ? 'icon' : null} onClick={() => setPage('notifs')}>{<FaBell />}  </li>
+        <li title="messages" id={(page === 'DMs') ? 'icon' : null} onClick={() => setPage('DMs')}>{<FaEnvelope />}  </li>
+        <li title="log out" onClick={() => axios.get('/logout')}>{<FaDoorOpen />}</li>
+        <li> <div className="wrap">
+              <div className="search">
+                <input type="text" className="search-term" placeholder="What are you watching?"/>
+                <button type="submit" className="search-button">
+                  <i className="FaSearch" ></i>
+                </button>
+              </div>
+            </div>
+        </li>
+      </ul>
       <div>{loadBody()}</div>
     </div>
   );
