a79bd3f82d7d8b1d3987cced219d3cda3c86e038
Andrew Gremillion <agremillion15@gmail.com>
2020-12-02 12:40:29 -0600

    (fix) searched shows summaries to now correctly show bold and italicized words

--- client/src/components/searchEntry.jsx
+++ client/src/components/searchEntry.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 const SearchFeedEntry = ({ show }) => {
-  const getSummary = () => show.summary.replace(/<.>|<\/.>/g, '');
+  const getSummary = () => {
+    let summary = show.summary.replace(/<p>|<\/p>/g, '');
+    const output = [];
+    while (summary.length > 0) {
+      if (summary.search(/<i>/) !== -1) {
+        output.push(summary.slice(0, summary.search(/<i>/)));
+        summary = summary.slice(summary.search(/<i>/) + 3);
+
+        const italic = summary.slice(0, summary.search(/<\/i/));
+        output.push(<i key={italic}>{italic}</i>);
+        summary = summary.slice(summary.search(/<\/i>/) + 4);
+      } else if (summary.search(/<b>/) !== -1) {
+        output.push(summary.slice(0, summary.search(/<b>/)));
+        summary = summary.slice(summary.search(/<b>/) + 3);
+
+        const bold = summary.slice(0, summary.search(/<\/b/));
+        output.push(<b key={bold}>{bold}</b>);
+        summary = summary.slice(summary.search(/<\/b>/) + 4);
+      } else {
+        output.push(summary);
+        summary = '';
+      }
+    }
+
+    return output;
+  };
   const getImage = () => {
     if (show.image !== null) {
       return show.image.medium;
