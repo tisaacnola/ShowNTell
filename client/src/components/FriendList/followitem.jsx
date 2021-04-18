ad9f2e17606c759e4baa2a1feb506b40c607caa6
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-04-17 21:30:02 -0500

    friends movie list renders

--- client/src/components/FriendList/followitem.jsx
+++ client/src/components/FriendList/followitem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import axios from 'axios';
@@ -8,13 +9,18 @@ class FollowItem extends Component {
     this.state = {
       subs: [],
       showSubs: [],
+      movies: [],
+      showMovies: [],
     };
     this.getSubs = this.getSubs.bind(this);
+    this.getMovies = this.getMovies.bind(this);
   }
 
   componentDidMount() {
     this.setState({ subs: this.props.friend.subscriptions });
+    this.setState({ movies: this.props.friend.movieSubscriptions });
     this.getSubs();
+    this.getMovies();
   }
 
   getSubs() {
@@ -29,13 +35,26 @@ class FollowItem extends Component {
       .catch();
   }
 
+  getMovies() {
+    console.log(this.props.friend.movieSubscriptions);
+    const promises = this.props.friend.movieSubscriptions.map((movieId) => axios.get(`movie/${movieId}`).catch());
+    Promise.all(promises)
+      .then((results) => results.map((movie) => movie.data))
+      .then((movies) => {
+        console.log(movies);
+        this.setState({ showMovies: movies });
+      })
+      .catch();
+  }
+
   render() {
     const { friend } = this.props;
-    const { showSubs } = this.state;
+    const { showSubs, showMovies } = this.state;
     return (
       <div>
         <h3 id="dms-sub-header">{ friend.name }</h3>
         { showSubs.map((show) => <span id={show.id} className="show-summary">{show.name}</span>) }
+        { showMovies.map((show) => <span id={show._id} className="show-summary">{show.title}</span>) }
       </div>
     );
   }
